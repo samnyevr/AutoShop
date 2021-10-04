21652a5a4aa5d45cca24fd42be9e4e2a53abe034
Samuel nye <samuelnyevr@gmail.com>
2021-10-04 00:37:51 -0700

    started services container

--- src/components/banner/styles/banner.js
+++ src/components/banner/styles/banner.js
@@ -2,14 +2,18 @@ import styled from "styled-components";
 
 export const Container = styled.section`
     display: flex;
+    justify-content: space-between;
+    background-image: linear-gradient(black, #1A1B1D, black);
 `;
 
 export const Image = styled.img`
     filter: invert(100%) sepia(0%) saturate(758%) hue-rotate(231deg) brightness(118%) contrast(108%);
+    width: 100px;
 `;
 
 export const Item = styled.section``;
 
 export const Text = styled.p`
     text-aligned = center;
+    width: 100px;
 `;
--- src/containers/services.js
+++ src/containers/services.js
@@ -1,7 +1,8 @@
 import React from "react"
 import {Banner} from "../components"
 
-/*Quick flat repairs
+/*
+Quick flat repairs
 Computerized tire balancing
 Tire rotation
 Wheel repair
@@ -17,7 +18,39 @@ export default function ServicesContainer({children}) {
         <Banner>
             <Banner.Item>
                 <Banner.Image src="../images/wheel.svg"></Banner.Image>
-                <Banner.Text>Tire Repair</Banner.Text>
+                <Banner.Text>Quick Flat Repairs</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Computerized Tire Balancing</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Tire Rotation</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Wheel Repair</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Tire Inspection</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Air and Pressure checks</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Tire Matching</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>Tire Mounting</Banner.Text>
+            </Banner.Item>
+            <Banner.Item>
+                <Banner.Image src="../images/wheel.svg"></Banner.Image>
+                <Banner.Text>New Tire Installation</Banner.Text>
             </Banner.Item>
         </Banner>
     )
