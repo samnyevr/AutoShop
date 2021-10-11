50f7375ee6982424b0afacd05402b9b468a00a7d
Samuel nye <samuelnyevr@gmail.com>
2021-10-11 03:43:48 -0700

    netlify test 2

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
     filename: "bundle.js",
     assetModuleFilename: "images/[hash][ext][query]",
   },
